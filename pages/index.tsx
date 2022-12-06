f4f671e6fda6907aaec159139e7f7eaa3865704f
Real-Bird <marbil815@gmail.com>
2022-12-06 19:35:36 +0900

    22-12-06 intersection observer infinite scroll

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const ListItem = ({ fruit }: { fruit: string }) => {
   const target = useRef<HTMLDivElement>(null);
@@ -37,22 +37,72 @@ const ListItem = ({ fruit }: { fruit: string }) => {
   );
 };
 
+const fakeFetch = (delay = 1000) =>
+  new Promise((res) => setTimeout(res, delay));
+
+const fruits = [
+  "apple",
+  "banana",
+  "orange",
+  "lemon",
+  "lime",
+  "pure",
+  "peach",
+  "berry",
+];
+
+const nextItem = [
+  "dorian",
+  "mango",
+  "starfruit",
+  "dragonFruit",
+  "almond",
+  "walnut",
+  "grape",
+  "persimmon",
+];
 export default function Home() {
-  const fruits = [
-    "apple",
-    "banana",
-    "orange",
-    "lemon",
-    "lime",
-    "pure",
-    "peach",
-    "berry",
-  ];
+  const target = useRef<HTMLDivElement>(null);
+  const [state, setState] = useState<{ item: string[]; isLoading: boolean }>({
+    item: [...fruits],
+    isLoading: false,
+  });
+  const fetchItems = async (nextItem: string[]) => {
+    setState((prev) => ({
+      ...prev,
+      isLoading: true,
+    }));
+    await fakeFetch();
+    setState((prev) => ({
+      item: [...prev.item, ...nextItem],
+      isLoading: true,
+    }));
+  };
+  useEffect(() => {
+    let observer: IntersectionObserver;
+    if (target) {
+      observer = new IntersectionObserver(
+        async ([e], observer) => {
+          if (e.isIntersecting) {
+            observer.unobserve(e.target);
+            await fetchItems(nextItem);
+            observer.observe(e.target);
+          }
+        },
+        { threshold: 0.5 }
+      );
+      observer.observe(target.current as Element);
+    }
+    return () => observer.disconnect();
+  }, [target]);
+
+  const { item, isLoading } = state;
   return (
     <div>
-      {fruits.map((fruit, i) => (
-        <ListItem key={i} fruit={fruit} />
-      ))}
+      {item.map((fruit, i) => {
+        return <ListItem key={i} fruit={fruit} />;
+      })}
+      <div ref={target}>{isLoading && "Loading..."}</div>
     </div>
   );
 }
